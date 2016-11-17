3cf9748791e9cc3bf9a12c3f0de61e8b715edf9c
Jordi Prats <jprats@systemadmin.es>
2016-11-17 17:35:32 +0100

    bugfix

--- manifests/account.pp
+++ manifests/account.pp
@@ -9,6 +9,6 @@ define dovecot::account (
 
   exec { "add passwd-file ${account}":
     command => "bash -c 'echo \"${account}:$(doveadm pw -s sha1 -p ${password} | cut -d '}' -f2)\" >> ${passwdfile}'",
-    unless => "grep \"${account}:$(doveadm pw -s sha1 -p ${password} | cut -d '}' -f2)\" ${passwdfile}",
+    unless  => "bash -c 'grep \"${account}:$(doveadm pw -s sha1 -p ${password} | cut -d '}' -f2)\" ${passwdfile}'",
   }
 }
