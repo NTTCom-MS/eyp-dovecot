4ee3e9a7a98f68703a314d8745c448017f97e16e
Jordi Prats <jprats@systemadmin.es>
2016-11-18 11:39:11 +0100

    mkdir base_dir

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 0.1.1
 
 * added socket variable for **dovecot::auth::unix-listener**
+* base_dir management
 
 ## 0.1.0
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,21 @@ class dovecot::install inherits dovecot {
     package { $dovecot::params::package_name:
       ensure => $dovecot::package_ensure,
     }
+
+    exec { "mkdir eyp-dovecot ${dovecot::base_dir}":
+      command => "mkdir -p ${dovecot::base_dir}",
+      creates => ${dovecot::base_dir},
+      require => Package[$dovecot::params::package_name],
+    }
+
+    # drwxr-xr-x 5 root dovecot 700 Nov 17 16:47 /var/run/dovecot/
+    file { $dovecot::params::package_name:
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'dovecot',
+      mode    => '0755',
+      require => Exec["mkdir eyp-dovecot ${dovecot::base_dir}"],
+    }
   }
 
 }
