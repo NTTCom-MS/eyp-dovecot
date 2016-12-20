2f52b4771fd6e05e6a486be9209dc65fc9df9a51
Jordi Prats <jprats@systemadmin.es>
2016-12-20 12:21:04 +0100

    canvi ordre

--- manifests/imaplogin.pp
+++ manifests/imaplogin.pp
@@ -12,10 +12,5 @@ class dovecot::imaplogin(
     order   => '40',
     content => template("${module_name}/imaplogin/imaplogin.erb"),
   }
-
-  concat::fragment{ '/etc/dovecot/dovecot.conf imap opts':
-    target  => '/etc/dovecot/dovecot.conf',
-    order   => '01',
-    content => template("${module_name}/imap/imap_opts.erb"),
-  }
+  
 }
