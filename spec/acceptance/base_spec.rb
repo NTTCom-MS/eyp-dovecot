f02b96d13bcb57f86f534929bf9595866d40cc4c
Jordi Prats <jprats@systemadmin.es>
2017-03-27 17:30:03 +0200

    check banner

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -9,12 +9,29 @@ describe 'dovecot class' do
       pp = <<-EOF
 
       class { 'dovecot': }
+    	class { 'dovecot::userdb': }
+    	class { 'dovecot::passdb': }
+    	class { 'dovecot::auth': }
+    	class { 'dovecot::auth::unixlistener': }
+    	class { 'dovecot::imaplogin': }
+
+    	dovecot::account { 'jordi@prats.cat':
+    	  password => 'demopassw0rd',
+    	}
 
       EOF
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it "sleep 10 to make sure dovecot is started" do
+      expect(shell("sleep 10").exit_code).to be_zero
+    end
+
+    it "check banner" do
+      expect(shell("echo | nc localhost 143 | grep 'ready to rock'").exit_code).to be_zero
     end
 
   end
