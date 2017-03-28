651823c1f65c54a49ee7ca19ffdea2cc211610e2
Jordi Prats <jprats@systemadmin.es>
2017-03-28 15:55:25 +0200

    wordkarround ubuntu 14

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -4,17 +4,14 @@ require_relative './version.rb'
 describe 'dovecot class' do
 
   context 'basic setup' do
+
+    it "workarround for ubuntu14.04 docker image" do
+      expect(shell("lsb_release -a | grep trusty; if [ $? -eq 0 ]; then mv /sbin/initctl /sbin/oldinitctl; echo -e '#!/bin/bash\nif [ $1 == \"--version\" ]\nthen\n  echo \"initctl (upstart 1.12.1)\"\nfi\n/sbin/oldinitctl \"$@\"' > /sbin/initctl; chmod 755 /sbin/initctl; fi").exit_code).to be_zero
+    end
+
     it "vmail dir" do
       expect(shell("bash -c 'mkdir -p /var/vmail; chmod 777 /var/vmail'").exit_code).to be_zero
     end
-
-    #/etc/puppetlabs/code/environments/production/modules
-    apply_manifest_opts = {
-      :catch_failures => true,
-      # I seem to need this otherwise Puppet doesn't pick up the required modules.
-      # :modulepath     => '/etc/puppetlabs/code/modules',
-      :debug          => true,
-    }
 
     # Using puppet_apply as a helper
     it 'should work with no errors' do
@@ -38,8 +35,8 @@ describe 'dovecot class' do
       EOF
 
       # Run it twice and test for idempotency
-      expect(apply_manifest(pp, apply_manifest_opts).exit_code).to_not eq(1)
-      expect(apply_manifest(pp, apply_manifest_opts).exit_code).to eq(0)
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
     end
 
     it "sleep 10 to make sure dovecot is started" do
