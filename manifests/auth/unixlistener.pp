808289e77ef9bef1b8d6509fe598cfe0e0d45aa9
Jordi Prats <jprats@systemadmin.es>
2017-03-28 12:26:36 +0200

    canvi defaults unix listener

--- manifests/auth/unixlistener.pp
+++ manifests/auth/unixlistener.pp
@@ -5,11 +5,13 @@
 #     user = postfix
 # }
 class dovecot::auth::unixlistener(
-                                    $user   = 'postfix',
-                                    $group  = 'postfix',
+                                    $user   = $dovecot::default_login_user,
+                                    $group  = $dovecot::mail_access_groups,
                                     $mode   = '0660',
                                     $socket = 'auth-client',
   ) inherits dovecot::params {
+
+  include ::dovecot
 
   concat::fragment{ '/etc/dovecot/dovecot.conf auth unixlistener':
     target  => '/etc/dovecot/dovecot.conf',
